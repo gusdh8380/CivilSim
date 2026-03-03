36aa69fcb8a00829c427c7ad1f013bdc9f4a8bf5
Hyeon O_Windows <gusdh2510@gmail.com>
2026-03-03 19:07:41 +0900

    feat : reportPanelUI 구현

---
+++ Assets/Scripts/UI/ReportPanelUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+using CivilSim.Core;
+using CivilSim.Economy;
+using CivilSim.Population;
+
+
+public class ReportPanelUI : MonoBehaviour
+{
+    [Header("패널 루트 (자식 오브젝트)")]
+    [SerializeField] private GameObject _panel;
+
+    [Header("열기/닫기 버튼 (미할당 시 자동 탐색)")]
+    [SerializeField] private Button _openButton;
+    [SerializeField] private Button _closeButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
